b3ea8d0ab253df2c096f078376e74940f01bd82e
Maxim Kovrov <maksim.kovrov@gmail.com>
2021-08-25 16:48:20 +0300

    Add workspace for Rofi command
    Add workspace name
    when create Rofi menu

--- cmd/rofi.go
+++ cmd/rofi.go
@@ -11,7 +11,9 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"unicode/utf8"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.i3wm.org/i3/v4"
@@ -19,7 +21,7 @@ import (
 
 type (
 	window struct {
-		ID               int64               `json:"id"`
+		ID               i3.NodeID           `json:"id"`
 		Name             string              `json:"name"` // window: title, container: internal name
 		Type             string              `json:"type"`
 		Urgent           bool                `json:"urgent"` // urgency hint set
@@ -32,12 +34,15 @@ type (
 const (
 	classColumnWidthSett = "width-class"
 	titleColumnWidthSett = "width-title"
+
+	// scratchName = "♺"
+	scratchName = ""
 )
 
 // rofiCmd represents the rofi command
 var rofiCmd = &cobra.Command{
 	Use:   "rofi",
-	Short: "Output windows list for rofi switcher or switch window by user's choice in done in rofi",
+	Short: "Output windows list for rofi switcher or switch window by user's choice was done in rofi",
 	Long: `This command ready to using in pipe. For example, you can show window list in rofi for choosing:
     bindsym $mod+Tab exec --no-startup-id bash -c 'i3qws rofi | rofi -dmenu -p window | i3qws rofi'
 If standart input is not a named pipe, command formats and output windows list.
@@ -83,11 +88,31 @@ func spaceAlign(orig string, max uint) string {
 	if len(rr) < int(max) {
 		rr = append(rr, []rune(strings.Repeat(" ", int(max)-len(rr)))...)
 	}
-	rr = rr[0 : max-1]
+	rr = rr[0:max]
 	return string(rr)
 }
 
 func outputMenu(ctx context.Context) error {
+	tree, err := i3.GetTree()
+	if err != nil {
+		return err
+	}
+
+	mapping := make(map[i3.NodeID]string)
+	fillFromNode(mapping, tree.Root)
+	maxLenWsp := 0
+	for k, v := range mapping {
+		if v == "__i3_scratch" {
+			mapping[k] = scratchName
+			v = scratchName
+		}
+
+		l := utf8.RuneCountInString(v)
+		if l > maxLenWsp {
+			maxLenWsp = l
+		}
+	}
+
 	str, err := getURL(ctx, listURL)
 	if err != nil {
 		return err
@@ -99,6 +124,19 @@ func outputMenu(ctx context.Context) error {
 		return err
 	}
 
+	titleMaxWidth := 0
+	for _, w := range ww {
+		n := strings.TrimSpace(w.Name)
+		l := utf8.RuneCountInString(n)
+		if l > titleMaxWidth {
+			titleMaxWidth = l
+		}
+	}
+
+	if titleMaxWidth > 0 && titleMaxWidth > viper.GetInt(titleColumnWidthSett) {
+		titleMaxWidth = viper.GetInt(titleColumnWidthSett)
+	}
+
 	for i, w := range ww {
 		marks := strings.Builder{}
 		for _, m := range w.Marks {
@@ -106,11 +144,40 @@ func outputMenu(ctx context.Context) error {
 			marks.WriteString(m)
 			marks.WriteString("]")
 		}
-		fmt.Fprintf(os.Stdout, "%3d %s %s %s\n", i,
+
+		wsp := mapping[w.ID]
+
+		fmt.Fprintf(os.Stdout, "%3d  %s  %s %s %s\n", i,
+			spaceAlign(wsp, uint(maxLenWsp)),
 			spaceAlign(w.WindowProperties.Class, viper.GetUint(classColumnWidthSett)),
-			spaceAlign(w.Name, viper.GetUint(titleColumnWidthSett)),
+			spaceAlign(strings.TrimSpace(w.Name), uint(titleMaxWidth)),
 			marks.String())
 	}
 
 	return err
+}
+
+// getWspWindowMap returns map windowID ⇒ workspace name
+func fillFromNode(mapping map[i3.NodeID]string, node *i3.Node) {
+	doInWorkspace(mapping, "", node)
+}
+
+func doInWorkspace(mapping map[i3.NodeID]string, workspace string, node *i3.Node) {
+	switch node.Type {
+	case i3.WorkspaceNode:
+		if workspace != "" {
+			logrus.Warnf("Workspace '%s' put in another '%s'. We shall using nested instead.", node.Name, workspace)
+		}
+		workspace = node.Name
+	case i3.Con:
+		mapping[node.ID] = workspace
+	}
+
+	for _, n := range node.Nodes {
+		doInWorkspace(mapping, workspace, n)
+	}
+
+	for _, n := range node.FloatingNodes {
+		doInWorkspace(mapping, workspace, n)
+	}
 }
